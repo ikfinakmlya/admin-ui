1103208d67887f3cafd48129f286c598cd82ba4b
ikfinakmlya <ikfina.kamaliya@gmail.com>
2024-10-08 20:23:17 +0700

    membuat page sign in dan sign up

---
+++ src/components/Elements/Button/index.jsx
@@ -0,0 +1,23 @@
+// const Button = (props) => {
+//   const {children, variant = "bg-black"} = props;
+
+//   return (
+//     <button className={'h-10 px-6 rounded-md text-sm ${variant}'} type="submit">
+//         {children}
+//     </button>
+//   );
+// };
+
+// export default Button;
+
+const Button = (props) => {
+  const { children, variant = "bg-black" } = props;
+
+  return (
+    <button className={`h-10 px-6 rounded-md text-sm ${variant}`} type="submit">
+      {children}
+    </button>
+  );
+};
+
+export default Button;

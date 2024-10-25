3c6176b5ebf48cdcaa811d7a27395d3d291e5062
ikfinakmlya <ikfina.kamaliya@gmail.com>
2024-10-25 15:40:08 +0700

    icons

--- src/components/Fragments/Header.jsx
+++ src/components/Fragments/Header.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
+import Input from "../Elements/LabeledInput/Input";
+import NotificationsIcon from "@mui/icons-material/Notifications";
 
-export const Header = () => {
+const Header = () => {
+  const date = new Date().toDateString().slice(4);
+
   return (
-    <header className="border-b-2 p-6 flex items-center justify-between">
-  <div className="flex">
-    <div className="font-bold text-lg">Username</div>
-    <div className="ms-6 text-gray-03">Oct 17, 2024</div>
-  </div>
-  <div className="flex">
-    <div>Icon</div>
-    <div className="ms-10 hidden sm:block">Search Box</div>
-  </div>
-</header>
-  )
-}
+    <header className="border-b-2 py-4 px-6 flex items-center justify-between">
+      <div className="flex">
+        <div className="font-bold text-lg">Username</div>
+        <div className="ms-6 text-gray-03">{date}</div>
+      </div>
+      <div className="flex">
+        <div className="self-center">
+          <NotificationsIcon className="text-gray-01 scale-110" />
+        </div>
+        <div className="ms-10 hidden sm:block">
+          <Input variant="bg-white w-80" />
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
--- src/components/Fragments/Navbar.jsx
+++ src/components/Fragments/Navbar.jsx
@@ -1,45 +1,95 @@
 import { Link } from "react-router-dom";
+import { Icon } from "../Elements/Icon"; // Pastikan file dan komponen Icon ada
+import Logo from "../Elements/Logo";
 
 const Navbar = () => {
+  const menus = [
+    {
+      id: "overview",
+      link: "/",
+      icon: <Icon.Overview />,
+      label: "Overview",
+    },
+    {
+      id: "balance",
+      link: "/balance",
+      icon: <Icon.Balance />,
+      label: "Balance",
+    },
+    {
+      id: "transaction",
+      link: "/transaction",
+      icon: <Icon.Transaction />,
+      label: "Transaction",
+    },
+    {
+      id: "bill",
+      link: "/bill",
+      icon: <Icon.Bill />,
+      label: "Bill",
+    },
+    {
+      id: "expenses",
+      link: "/expenses",
+      icon: <Icon.Expenses />,
+      label: "Expenses",
+    },
+    {
+      id: "goal",
+      link: "/goal",
+      icon: <Icon.Goal />,
+      label: "Goal",
+    },
+    {
+      id: "setting",
+      link: "/setting",
+      icon: <Icon.Setting />,
+      label: "Setting",
+    },
+  ];
+
   return (
-    <nav className="bg-defaultBlack text-special-bg2 sm:w-72 w-36 h-screen px-7 py-12 flex flex-col justify-between">
-  <div>
-    <Link to="/">  
-    <div className="flex justify-center mb-10">Logo</div>
-    <div className="flex bg-primary text-white px-4 py-3 rounded-md">
-      <div className="text-white mx-auto sm:mx-0">A</div>
-      <div className="ms-3 text-white hidden sm:block">Overview</div>
-    </div>
-    </Link>
-    <Link to="/balance">
-    <div className="flex hover:bg-special-bg3 px-4 py-3 rounded-md">
-      <div className="mx-auto sm:mx-0">B</div>
-      <div className="ms-3 text-special-bg2 hidden sm:block">Balance</div>
-    </div>
-    </Link>
-    <div className="flex hover:bg-special-bg3 px-4 py-3 rounded-md">
-      <div className="mx-auto sm:mx-0">C</div>
-      <div className="ms-3 text-special-bg2 hidden sm:block">Transaction</div>
-    </div>
-  </div>
-  <div className="sticky bottom-12">
-    <div className="flex bg-special-bg3 px-4 py-3 rounded-md">
-      <div className="mx-auto sm:mx-0">D</div>
-      <div className="ms-3 text-special-bg2 hidden sm:block">Logout</div>
-    </div>
-    <div className="border-b my-10 border-b-special-bg"></div>
-    <div className="flex justify-between">
-      <div className="mx-auto sm:mx-0">foto</div>
-      <div className="hidden sm:block">
-        Username
-        <br />
-        View Profile
+    <nav className="bg-defaultBlack text-special-bg2 sm:w-72 w-28 min-h-screen px-7 py-12 flex flex-col justify-between">
+      <div>
+        <div className="flex justify-center mb-10">
+          <Logo variant="text-white text-sm sm:text-2x1" />
+        </div>
+        {menus.map((menu) => (
+          <Link key={menu.id} to={menu.link}>
+            <div className="flex hover:bg-special-bg3 hover:text-white px-4 py-3 rounded-md">
+              <div className="mx-auto sm:mx-0">{menu.icon}</div>
+              <div className="ms-3 hidden sm:block">{menu.label}</div>
+            </div>
+          </Link>
+        ))}
       </div>
-      <div className="hidden sm:block">icon</div>
-    </div>
-  </div>
-</nav>
-  )
-}
+      <div className="sticky bottom-12">
+        {/* Bagian Logout */}
+        <Link to="/logout">
+          <div className="flex bg-special-bg3 px-4 py-3 rounded-md">
+            <div className="mx-auto sm:mx-0">
+              <Icon.Logout />
+            </div>
+            <div className="ms-3 hidden sm:block">Logout</div>
+          </div>
+        </Link>
+        <div className="border-b my-10 border-b-special-bg"></div>
+        {/* Bagian Profil */}
+        <div className="flex justify-between">
+          <div className="mx-auto sm:mx-0 self-center">
+            <img src="images/profile.png" alt="Profile" />
+          </div>
+          <div className="hidden sm:block">
+            <div className="text-white font-bold">Username</div>
+            <div className="text-xs">View Profile</div>
+          </div>
+          <div className="hidden sm:block self-center justify-end">
+            <Icon.KebabMenu />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;

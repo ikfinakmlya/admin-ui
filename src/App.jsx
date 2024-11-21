41f7baffa784d6dc742566992397ba2e3abfa5d1
ikfinakmlya <112888653+ikfinakmlya@users.noreply.github.com>
2024-11-22 00:20:41 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import SignInPage from "./pages/signIn";
-import SignUpPage from "./pages/signUp";
+import SignInPage from "./pages/SignIn";
+import SignUpPage from "./pages/SignUp";
 import ErrorRoute from "./pages/errorRoute";
 import ForgotPasswordPage from "./pages/forgotPassword";
 import DashboardPage from "./pages/dashboard";

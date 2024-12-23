b418c6c8f5cfacc9cbe86c8bece8e9adc88791df
ikfinakmlya <112888653+ikfinakmlya@users.noreply.github.com>
2024-12-23 15:47:51 +0700

    Add files via upload

--- src/components/Fragments/FormSignIn.jsx
+++ src/components/Fragments/FormSignIn.jsx
@@ -1,30 +1,29 @@
-import LabeledInput from '../Elements/LabeledInput'
-import CheckBox from '../Elements/CheckBox'
-import Button from '../Elements/Button/index'
+import LabeledInput from '../Elements/LabeledInput';
+import CheckBox from '../Elements/CheckBox';
+import Button from '../Elements/Button/index';
 import { useForm } from "react-hook-form";
-import axios from 'axios';
-import { useContext, useState } from "react";
-import CustomizedSnackbars from "../Elements/SnackBar";
+import axios from "axios";
+import {useContext, useState} from "react";
+import CustomizedSnackbars from '../Elements/SnackBar';
 import { jwtDecode } from 'jwt-decode';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../context/authContext';
 
+
 const FormSignIn = () => {
-  const [msg, setMsg] = useState("");
-  const [open, setOpen] = useState (true);
-  const [ setIsLoggedIn, setName] = useContext(AuthContext);
+  const [msg,setMsg] = useState("");
+  const [open, setOpen] = useState(true);
+  const { setIsLoggedIn, setName } = useContext(AuthContext);
 
   const navigate = useNavigate();
 
   const {
     register,
     handleSubmit,
-    formState: {errors, isValid},
-  } = useForm ({
+    formState: { errors,isValid },
+  } = useForm({
     mode: "onChange",
   });
-
-  const onErrors  = (errors) => console.error(errors);
 
   const onFormSubmit = async (data) => {
     try {
@@ -35,90 +34,87 @@ const FormSignIn = () => {
           password: data.password,
         }
       );
-  
-      const decoded = jwtDecode(response.data.refreshToken);
-      console.log(decoded);
       
-      // console.log(response);
+      const decode = jwtDecode(response.data.refreshToken);
+      //console.log(decode);
+
+      setOpen(true);
+      setMsg({ severty: "success", desc: "Login Success"});
+
+      localStorage.setItem("refreshToken", response.data.refreshToken);
+
+      setIsLoggedIn(true);
+      setName(decode.name);
+
+      navigate("/");
+
+    } catch (error) {
+      if (error.response) {
         setOpen(true);
-        setMsg({ severity: "success", desc: "Login success" });
-
-        localStorage.setItem("refreshToken", response.data.refreshToken);
-
-        setIsLoggedIn(true);
-        setName(decoded.name);
-
-        navigate("/");
-
-      } catch (error) {
-        if (error.response) {
-          setOpen(true);
-          setMsg({ severity: "error", desc: error.response.data.msg });
-        }
+        setMsg( {severity: "error", desc : error.response.data.msg});
       }
+    }
   };
-
-  
+  const onErrors = (errors) => console.error(errors);
 
   return (
     <form onSubmit={handleSubmit(onFormSubmit, onErrors)}>
-        <div className="mb-6">
+      <div className="mb-6">
         <LabeledInput
-  label="Email address"
-  type="email"
-  placeholder="hello@example.com"
-  name="email"
-  register={{
-    ...register("email", {
-      required: "Email address is required",
-      pattern: {
-        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-        message: "Invalid email address format",
-      },
-    }),
-  }}
-/>
-{errors?.email && (
-  <div className="text-center text-red-500">{errors.email.message}</div> 
-)}
-        </div>
-        <div className="mb-6">
+          label="Email address"
+          type="email"
+          placeholder="hello@example.com"
+          name="email"
+          register={register("email", {
+            required: "Email address is required",
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              message: "Invalid email address format",
+            },
+          })}
+        />
+        {errors?.email && (
+          <div className="text-center text-red-500">{errors.email.message}</div>
+        )}
+      </div>
+      <div className="mb-6">
         <LabeledInput
-            label="Password"
-            type="password"
-            placeholder="*************"
-            name="password"
-            register={{
-              ...register("password", {required: "Password is required"}),
-            }}
+          label="Password"
+          type="password"
+          placeholder="*************"
+          name="password"
+          register={register("password", {
+            required: "Password is required",
+          })}
         />
         {errors?.password && (
-          <div className="text-center text-red-500">{errors.password.message}</div> 
+          <div className="text-center text-red-500">{errors.password.message}</div>
         )}
-        </div>
-        <div className="mb-3">
+      </div>
+      <div className="mb-3">
         <CheckBox label="Keep me signed in" name="status" />
-        </div>
-        <Button
-        variant={
-          !isValid
+      </div>
+      <Button variant={
+        !isValid 
           ? "bg-gray-05 w-full text-white"
           : "bg-primary w-full text-white"
-        }
-         type="submit"
-         disabled={isValid ? "disabled" : ""}>
-            Login
-        </Button>
-        {msg && (
-          <CustomizedSnackbars
-          severity={msg.severity}
+      } 
+      type="submit"
+      disable={!isValid ? "disable" : ""}
+      >
+        Login
+      </Button>
+      {msg && (
+        <CustomizedSnackbars
+          severty={msg.severty}
           message={msg.desc}
           open={open}
           setOpen={setOpen}
         />
-        )}
+      )}
+      {/* <div className="mt-3 text-center text-red-500">{msg}</div> */}
     </form>
   );
-}
+};
 
-export default FormSignIn
+export default FormSignIn;
